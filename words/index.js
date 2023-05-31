b35c0dd3b5114b178a083879b3a4711a5cbe9746
and4po <94716615+and4po@users.noreply.github.com>
2023-05-31 15:08:06 +0200

    Correcció d'una errada del codi js

--- words/index.js
+++ words/index.js
@@ -2,8 +2,8 @@ function canvia() {
     const text_area = document.getElementById("input")
     var str = text_area.value
     if (str == "") {
-        document.getElementById("number").innerHTML = "Inseriu un text per comptar-ne les paraules."
-    } else {
         document.getElementById("number").innerHTML = ""
+    } else {
+        document.getElementById("number").innerHTML = "El text és de  " + str.split(' ').length + " paraules."
     }
   }
